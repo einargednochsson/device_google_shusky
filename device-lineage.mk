e9e80a82fc21d3ed568643e1fca870a1dbbc239c
Matt Filetto <matt.filetto@gmail.com>
2024-01-12 20:26:31 -0500

    shusky: Use google's face unlock implementation

--- device-lineage.mk
+++ device-lineage.mk
@@ -30,6 +30,9 @@ include hardware/google/pixel/powershare/device.mk
 # wireless_charger HAL service
 include device/google/gs-common/wireless_charger/wireless_charger.mk
 
+# Face Unlock
+include vendor/google/faceunlock/device.mk
+
 # Build necessary packages for vendor
 
 # Audio
